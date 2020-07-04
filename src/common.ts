5aa6e1ce2e0744658a75aa1083651477165c385d
José San Gil <jsangilve@gmail.com>
2020-07-04 13:11:55 +0200

    fix: move parseFormData to a common module

---
+++ src/common.ts
@@ -0,0 +1,66 @@
+
+import * as Busboy from 'busboy';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+export interface UploadedFile {
+  filename: string;
+  contentType: string;
+  encoding: string;
+  content: Buffer | string;
+}
+
+export interface FormData {
+  file: UploadedFile;
+  fields: Record<string, any>;
+}
+
+/** 
+ * Parses the multipart form data and returns the uploaded files and fields
+ */
+export const parseFormData = async (
+  event: APIGatewayProxyEvent,
+): Promise<FormData> =>
+  new Promise((resolve, reject) => {
+    const busboy = new Busboy({
+      headers: { 'content-type': event.headers['content-type'] },
+    });
+    const fields: Record<string, any> = {};
+    let uploadedFile: UploadedFile;
+
+    // event listener for the form data
+    busboy.on('file', (field, file, filename, encoding, contentType) => {
+      let content = null;
+
+      file.on('data', (data) => {
+        // reads the file content in one chunk
+        content = data;
+      });
+
+      file.on('error', reject);
+
+      file.on('end', () => {
+        uploadedFile = {
+          filename,
+          encoding,
+          contentType,
+          content,
+        };
+      });
+    });
+
+    busboy.on('field', (fieldName, value) => {
+      fields[fieldName] = value;
+    });
+
+    busboy.on('error', reject);
+
+    busboy.on('finish', () => {
+      if (!uploadedFile) {
+        reject(new Error('Missing file'));
+      }
+      resolve({ file: uploadedFile, fields })
+    });
+
+    busboy.write(event.body, event.isBase64Encoded ? 'base64' : 'binary');
+    busboy.end();
+  });
--- src/uploadFile.ts
+++ src/uploadFile.ts
@@ -1,74 +1,11 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda';
+import { APIGatewayProxyHandler } from 'aws-lambda';
 import { S3 } from 'aws-sdk';
 import * as queryString from 'querystring';
-import * as Busboy from 'busboy';
-
-interface UploadedFile {
-  filename: string;
-  contentType: string;
-  encoding: string;
-  content: Buffer | string;
-}
-
-interface FormData {
-  file: UploadedFile;
-  fields: Record<string, any>;
-}
+import { parseFormData } from './common';
 
 const s3Client = new S3();
 
 const BUCKET_NAME = 'serverless-example-bucket';
-
-/** 
- * Parses the multipart form data and returns the uploaded files and fields
- */
-const parseFormData = async (
-  event: APIGatewayProxyEvent,
-): Promise<FormData> =>
-  new Promise((resolve, reject) => {
-    const busboy = new Busboy({
-      headers: { 'content-type': event.headers['content-type'] },
-    });
-    const fields: Record<string, any> = {};
-    let uploadedFile: UploadedFile;
-
-    // event listener for the form data
-    busboy.on('file', (field, file, filename, encoding, contentType) => {
-      let content = null;
-
-      file.on('data', (data) => {
-        // reads the file content in one chunk
-        content = data;
-      });
-
-      file.on('error', reject);
-
-      file.on('end', () => {
-        uploadedFile = {
-          filename,
-          encoding,
-          contentType,
-          content,
-        };
-      });
-    });
-
-    busboy.on('field', (fieldName, value) => {
-      fields[fieldName] = value;
-    });
-
-    busboy.on('error', reject);
-
-    busboy.on('finish', () => {
-      if (!uploadedFile) {
-        reject(new Error('Missing file'));
-      }
-      resolve({ file: uploadedFile, fields })
-    });
-
-    busboy.write(event.body, event.isBase64Encoded ? 'base64' : 'binary');
-    busboy.end();
-  });
 
 export const uploadFile: APIGatewayProxyHandler = async (event) => {
   const { file, fields } = await parseFormData(event);
